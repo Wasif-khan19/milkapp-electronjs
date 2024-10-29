b865fd7a5ee302c1823dca1706b10c26b87c9611
Wasif-khan19 <wk5599426@gmail.com>
2024-10-29 22:41:13 +0500

    new

---
+++ src/renderer/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <div className="creator">Powered by STC</div>
+      <div className="text">
+        Welcome To Milk Testing Software<span className="react"></span>
+      </div>
+    </>
+  )
+}
+
+export default App
